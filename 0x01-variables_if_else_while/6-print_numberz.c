06536125204f15b4e5bff62d8d771b1d88312cd4
Michellezt <ndanuxalem@gmail.com>
2023-03-17 14:58:03 -0700

    putchar output in numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - starting point
+  *
+  * Return: 0 always
+  */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0 ; m < 10 ; m++)
+		putchar(m + '0');
+	putchar('\n');
+	return (0);
+}
