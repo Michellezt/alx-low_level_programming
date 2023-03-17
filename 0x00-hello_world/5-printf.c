cdaa23ad9e89acfe759284d31b14a693692a73d6
Michellezt <ndanuxalem@gmail.com>
2023-03-16 17:14:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point for program execution
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
